cce44f92aa27cc546167a0ee05431322dfcf21fe
Narciso Pardo <info@furgocasa.com>
2026-01-10 13:12:50 +0100

    Fix: Aumentar padding-top en pagina sitemap-html - Evitar que contenido se esconda detras del menu fijo - Cambiar py-8 a pt-24 pb-12

--- app/sitemap-html/page.tsx
+++ app/sitemap-html/page.tsx
@@ -56,7 +56,7 @@ export default function SitemapHTMLPage() {
   ]
 
   return (
-    <div className="min-h-screen bg-gray-50 py-8">
+    <div className="min-h-screen bg-gray-50 pt-24 pb-12">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         
         {/* Header con advertencia */}
